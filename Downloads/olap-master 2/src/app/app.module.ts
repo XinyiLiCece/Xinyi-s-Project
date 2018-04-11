190dedeed4c5c76e7007f301b237e8b4661c3f3e
wangshuang <xlzzd@mst.edu>
2018-04-10 23:49:32 -0400

    query6 query9

---
+++ Downloads/olap-master 2/src/app/app.module.ts
@@ -0,0 +1,84 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+
+
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatCardModule, MatChipsModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatSelectModule,
+  MatSnackBarModule,
+  MatToolbarModule
+} from '@angular/material';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { PageNotFoundComponent }   from './not-found.component';
+import { LoginComponent } from './component/login/login.component';
+import { QueryComponent } from './component/query/query.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { SqlComponent } from './component/sql/sql.component';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { PresentQuery1Component } from './component/present-query1/present-query1.component';
+import { PresentQuery2Component } from './component/present-query2/present-query2.component';
+import { PresentQuery4Component } from './component/present-query4/present-query4.component';
+import { PresentQuery8Component } from './component/present-query8/present-query8.component';
+import {HttpClientModule} from "@angular/common/http";
+import {DbService} from "./service/db.service";
+import {AuthService} from './service/auth-service';
+import { PresentQuery7Component } from './component/present-query7/present-query7.component';
+
+import { PresentQuery9Component } from './component/present-query9/present-query9.component';
+
+@NgModule({
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatSelectModule,
+    MatListModule,
+    MatSidenavModule,
+  ChartsModule,
+  FormsModule,
+
+  ],
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    LoginComponent,
+    QueryComponent,
+    SqlComponent,
+    PresentQuery1Component,
+    PresentQuery2Component,
+    PresentQuery4Component,
+    PresentQuery8Component,
+    PresentQuery7Component,
+   
+    PresentQuery9Component,
+    
+
+  ],
+  providers: [DbService, AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Downloads/olap-master 2/src/app/component/present-query2/present-query2.component.ts
@@ -0,0 +1,76 @@
+import { Component, AfterViewInit, ViewChild, ElementRef, Injectable} from '@angular/core';
+import { Chart } from 'chart.js';
+import { HttpClient} from "@angular/common/http";
+import {DbService} from "../../service/db.service";
+
+@Component({
+  selector: 'app-present-query2',
+  templateUrl: './present-query2.component.html',
+  styleUrls: ['./present-query2.component.css']
+})
+
+@Injectable()
+export class PresentQuery2Component implements AfterViewInit {
+  chart = [];
+  @ViewChild("mycanvas") mycanvas:ElementRef;
+  constructor(private http: HttpClient, private db:DbService) { }
+  dbversion:string;
+  data={labels:[], values:[]};
+
+  extract_data(data){
+    this.data.labels=[];
+    this.data.values=[];
+    for (let arr of data){
+      this.data.labels.push(arr.join_mode);
+      this.data.values.push(arr.avg_rsvp);
+    }
+  }
+  mock_data(){
+    this.data.labels=[1,2,3];
+    this.data.values=[4,5,6];
+  }
+  ngOnInit(){
+    console.log(123)
+    this.dbversion=this.db.getDbVersion();
+    this.http.get<any>(`https://adbm-final.herokuapp.com/api/database/${this.dbversion}/query/2`).subscribe(
+      data =>{
+        this.extract_data(data);
+        this.ngAfterViewInit();
+      
+      },
+      error =>{
+        console.log('error');
+        this.mock_data();
+        this.ngAfterViewInit();
+
+      }
+    )
+  }
+ 
+
+  ngAfterViewInit() {
+    //console.log(this.mycanvas);
+    var ctx =  this.mycanvas.nativeElement.getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.data.labels,
+        datasets: [
+          {
+            label: "RSVP",
+            backgroundColor: ["#3e95cd", "#8e5ea2"],
+            data: this.data.values
+          }
+        ]
+      },
+      options: {
+        legend: { display: true },
+        title: {
+          display: true,
+          text: 'Find different group join modes'
+        }
+      }
+    });
+  }
+
+}
